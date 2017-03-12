81a30152989364bd6030cbb1c6538f11428b9882
Hakon Hanesand <hakon@abello.no>
2017-03-12 03:14:12 -0500

    Remove comments

--- update.sh
+++ update.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-# reset_development_server() {
-
-# 	local devServiceFileName="dev-instacrated.service.txt"
-# 	local devServiceName="dev-instacrated.service"
-# 	local destinationPath="/etc/systemd/system/"
-
-# 	echo "\n>>>> sudo cp $devServiceFileName $destinationPath$devServiceName"
-# 	sudo cp "$devServiceFileName" "$destinationPath$devServiceName"
-
-# 	echo "\n>>>> sudo chmod 664 $destinationPath$devServiceName"
-# 	sudo chmod 664 "$destinationPath$devServiceName"
-
-# 	echo "\n>>>> systemctl daemon-reload"
-# 	systemctl daemon-reload
-
-# 	echo "\n>>>> systemctl restart $devServiceName"
-# 	systemctl restart "$devServiceName"
-# }
-
 reset_production_server() {
 
 	local prodServiceFileName="polymyrd.service.txt"
@@ -49,18 +30,10 @@ fi
 echo "\n>>>> vapor build --release=true --fetch=false"
 vapor build --release=true --fetch=false
 
-echo "\n>>>> sudo systemctl restart instacrated.service"
+echo "\n>>>> sudo systemctl restart polymyrd.service"
 sudo systemctl restart polymyrd.service
-
-# echo "\n>>>> sudo systemctl restart dev-instacrated.service"
-# sudo systemctl restart dev-instacrated.service
 
 if [[ $(git diff --name-only HEAD~1 HEAD -- polymyrd.service.txt) ]]; then
     echo "\n>>>> Detected changes in production server configuration files!"
 	reset_production_server
 fi
-
-# if [[ $(git diff --name-only HEAD~1 HEAD -- dev-instacrated.service.txt) ]]; then
-# 	echo "\n>>>> Detected changes in development server configuration files!"
-# 	reset_development_server
-# fi
