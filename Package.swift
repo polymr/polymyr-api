715fcfdd024641e757772ff729bf1f44b46e2c46
Hakon Hanesand <hakon@abello.no>
2017-03-30 09:02:48 -0500

    Update package

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "polymr-api",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1),
-        .Package(url: "https://github.com/nodes-vapor/sanitized.git", majorVersion: 0),
+        .Package(url: "https://github.com/vapor/vapor.git", Version(2, 0, 0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", Version(2, 0, 0, prereleaseIdentifiers: ["beta"])),
+//        .Package(url: "https://github.com/nodes-vapor/sanitized.git", majorVersion: 0),
     ],
     exclude: [
         "Config",
