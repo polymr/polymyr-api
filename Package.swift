089769524a66ba031acd9a5b035ab972274d613d
Hakon Hanesand <hakon@abello.no>
2017-04-16 00:59:51 -0500

    Update to fork of JWT.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", Version(2, 0, 0, prereleaseIdentifiers: ["beta"])),
         .Package(url: "https://github.com/vapor/mysql-provider.git", Version(2, 0, 0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/jwt.git", Version(2, 0, 0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/hhanesand/jwt.git", Version(2, 0, 0, prereleaseIdentifiers: ["beta"])),
         .Package(url: "https://github.com/hhanesand/auth-provider.git", Version(1, 0, 0, prereleaseIdentifiers: ["beta"]))
     ],
     exclude: [
